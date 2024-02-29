94a91c5233fd80b7fe3bf0b7b3ca2319ee40cb99
John Tatka <tatkajohn@gmail.com>
2024-02-28 18:02:15 -0800

    Initial Update For Part 1

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -16,3 +16,19 @@ CREATE TABLE Vaccines (
     Doses int,
     PRIMARY KEY (Name)
 );
+
+CREATE TABLE Patients (
+    Username varchar(255),
+    Salt BINARY(16),
+    Hash BINARY(16),
+    PRIMARY KEY (Username)
+);
+
+CREATE TABLE Appointments (
+    appointment_id int,
+    vac_name varchar(255) REFERENCES Vaccines(Name),
+    Time date,
+    patient_name varchar(255) REFERENCES Patients(Username),
+    caregiver_name varchar(255) REFERENCES Caregivers(Username),
+    PRIMARY KEY (appointment_id)
+);
